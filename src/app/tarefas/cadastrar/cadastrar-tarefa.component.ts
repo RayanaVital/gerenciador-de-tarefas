4ffabb3ce6885d366324e19f493b627114cc0dac
Rayana Vital <rayanavital@gmail.com>
2022-09-30 16:35:17 -0300

    Implementando validações e mensagens.

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { TarefaService } from './../shared/tarefa.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Tarefa } from '../shared/tarefa.model';
 
 @Component({
   selector: 'app-cadastrar-tarefa',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
+
+  tarefa: Tarefa;
+
+  constructor(private tarefaService: TarefaService, private router: Router) { }
 
   ngOnInit(): void {
+    this.tarefa = new Tarefa();
+  }
+
+  cadastrar(): void{
+    if(this.formTarefa.form.valid){ //caso o formulario seja valido ele prosseguira o cadastro
+      this.tarefaService.cadastrar(this.tarefa); //cadastra a tarefa e ira para a pagina de listagem
+      this.router.navigate(["/tarefas"]);
+    }
   }
 
 }
